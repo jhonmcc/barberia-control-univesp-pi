53ef2653c69e9bbbc39010cf509425c4e68d4bc2
unknown <desenv@DEV.portoseguro.brasil>
2026-04-08 21:23:16 -0300

    initial files

---
+++ barbeariaunivesp/src/main/java/com/univesp/barbearia/pi/BarbeariaunivespApplication.java
@@ -0,0 +1,13 @@
+package com.univesp.barbearia.pi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaunivespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaunivespApplication.class, args);
+	}
+
+}
---
+++ barbeariaunivesp/src/test/java/com/univesp/barbearia/pi/BarbeariaunivespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univesp.barbearia.pi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaunivespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
